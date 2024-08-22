9a30c6baad50a01d447108516562759747e1e2a3
vankhang49 <138227753+vankhang49@users.noreply.github.com>
2024-08-22 16:46:36 +0700

    Update NumberGame.jsx

--- src/component/NumberGame.jsx
+++ src/component/NumberGame.jsx
@@ -41,7 +41,7 @@ export function NumberGame() {
 
     const handleStartGame = () => {
         const number = parseInt(inputNumber);
-        if (isNaN(number) || number <= 0) {
+        if (isNaN(number) || number <= 0 || number > 1000) {
             setMessage('Vui lòng nhập một số hợp lệ lớn hơn 0.');
             return;
         }
@@ -125,6 +125,7 @@ export function NumberGame() {
                                 top: `${positions[index]?.y}%`,
                                 left: `${positions[index]?.x}%`,
                                 backgroundColor: hasFailed ? 'red' : correctNumbers.includes(number) ? '#5cea07' : '#ffcc00',
+                                z-index: {number - index}
                             }}
                         >
                             {number}
